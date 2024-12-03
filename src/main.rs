fafef519f56d6c7ea1b4c4907d9a02ced37ca019
Dennis Kobert <dennis@kobert.dev>
2024-12-03 16:09:56 +0100

    Add solution files

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::io::{BufReader, Cursor};
 use closest_string_assignment::*;
 
 fn main() {
+    let i = 4;
     let mut instance = parse_input(BufReader::new(
-        std::fs::File::open("string1.in").unwrap(),
+        std::fs::File::open(format!("string{i}.in")).unwrap(),
         // Cursor::new("4\nada\nabb\nacc\nbad".to_string()),
     ));
-    instance.shorten();
-    instance.dedup();
+    // instance.shorten();
+    // instance.dedup();
     // dbg!(&instance);
     // dbg!(instance.strings.len());
     // dbg!(instance.find_max_pairwise_d());
@@ -19,7 +20,11 @@ fn main() {
     // // dbg!(instance.restrict_instance_to_d_core(50).0.strings.len());
     // dbg!(instance.find_max_pairwise_d());
     instance.populate_tensor();
-    instance.solve();
+    let guess = instance.solve();
+    let chars: Vec<u8> = guess.into_iter().map(|x| x + b'a').collect();
+    let string = String::from_utf8(chars).unwrap();
+    println!("Solution: {string}");
+    std::fs::write(format!("string{i}.out"), string).unwrap();
     // dbg!(new_instance.restrict_instance_to_d_core(4).0.strings.len());
     // println!("Hello, world!\n{:?}", instance);
 }
